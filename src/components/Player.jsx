6a3a66f4266bfd07a5f09fb188fec37eac4f9dd1
Sergio-dev01 <sergio.develoop@gmail.com>
2025-05-30 17:28:35 +0200

    bg player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,7 +7,7 @@ const Player = () => {
   console.log("Current song in Player:", currentSong);
 
   return (
-    <div className="container-fluid fixed-bottom bg-container pt-1">
+    <div className="container-fluid fixed-bottom bg-container pt-1 bg-dark">
       <div className="row h-100">
         <div className="col-lg-10 offset-lg-2">
           <div className="row h-100 flex-column justify-content-center align-items-center">
